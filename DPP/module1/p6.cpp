5a33018faba6afd55c44c71373ecbf786be9c0da
Swaraj Shinde <officially.swaraj@gmail.com>
2026-07-11 23:59:25 +0530

    Add solution to 6-10 problem

---
+++ DPP/module1/p6.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long a,b,c;
+    cin>>a>>b>>c;
+    if(a<=b && a<=c)
+    {
+        cout<<a<<" ";
+    }
+    else if(b<=c && b<=a)
+    {
+        cout<<b<<" ";
+    }
+    else
+    {
+        cout<<c<<" ";
+    }
+
+    if(a>=b && a>=c)
+    {
+        cout<<a;
+    }
+    else if(b>=c && b>=a)
+    {
+        cout<<b;
+    }
+    else
+    {
+        cout<<c;
+    }
+    return 0;
+}
